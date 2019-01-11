46146ee70e3eefeba6f05b13761837bdb1de3cf8
huangyujie <190158792@qq.com>
2019-01-11 14:06:44 +0800

    mediun算法

---
+++ src/main/java/com/hyj/arithmetic/medium/MediumArithmetic.java
@@ -0,0 +1,10 @@
+package com.hyj.arithmetic.medium;
+
+/**
+ * @version 1.0
+ * @ Authord  yujie huang  email: 190158792@qq.com
+ * @ Description
+ * @ Date Create by in 14:04 2019/1/11
+ */
+public class MediumArithmetic {
+}
