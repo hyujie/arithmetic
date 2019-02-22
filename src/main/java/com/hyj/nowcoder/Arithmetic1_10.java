c9c5313bbd522e5411e162b031454d9eb325befd
huangyujie <190158792@qq.com>
2019-02-22 14:49:51 +0800

    算法1-10

--- src/main/java/com/hyj/nowcoder/Arithmetic1_10.java
+++ src/main/java/com/hyj/nowcoder/Arithmetic1_10.java
@@ -75,7 +75,7 @@ public class Arithmetic1_10 {
         int l = array.length;
         int i = 0;
         int j = l-1;
-        while(i<array.length&&j>0&&i<j){
+        while(i<j){
             if(array[i]>array[i + 1]){
                 System.out.println(i);
                 return array[i+1];
@@ -87,6 +87,218 @@ public class Arithmetic1_10 {
             j--;
         }
         return l == 0?0:array[j];
+    }
+
+
+    /**
+     * 递归法
+     * 大家都知道斐波那契数列，现在要求输入一个整数n，
+     * 请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+     * n<=39
+     * @param n
+     * @return
+     */
+    public static int Fibonacci(int n) {
+        //0,1,1,2,3,5,8........ f(n) = f(n-1) + f(n-2);
+        if( n == 1){
+            return 1;
+        }
+        if (n == 0){
+            return 0;
+        }
+        return Fibonacci(n - 2)+Fibonacci(n-1);
+
+    }
+
+    /**
+     * 循环法
+     * @param n
+     * @return
+     */
+    public static int Fibonacci2(int n) {
+        //0,1,1,2,3,5,8........ f(n) = f(n-1) + f(n-2);
+
+        int first = 0;
+        int second  =1;
+        int i = 2;
+        int result = 0;
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        while(i <= n){
+            result = first + second;
+            first = second;
+            second = result;
+            i++;
+        }
+        return result;
+    }
+
+    /**
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。
+     * 求该青蛙跳上一个n级的台阶总共有多少种跳法（先后次序不同算不同的结果）。
+     * @param target
+     * @return
+     */
+    public static int JumpFloor(int target) {
+        //1 2 3 5 8 13
+        if(target == 1){
+            return 1;
+        }
+        if(target == 2){
+            return 2;
+        }
+        int first = 1;
+        int second = 2;
+        int n = 3;
+        int result = 0;
+        while(n<=target){
+            result = first + second;
+            first = second;
+            second = result;
+            n++;
+        }
+        return result;
+    }
+
+    /**
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
+     * 求该青蛙跳上一个n级的台阶总共有多少种跳法。
+     * @param target
+     * @return
+     */
+    public static int JumpFloorII(int target) {
+        int n =1;
+        int result = 1;
+        while(n<target){
+            result *=2;
+            n++;
+        }
+        return result;
+    }
+
+    /**
+     * 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+     * 请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+     * @param target
+     * @return
+     */
+    public  static int RectCover(int target) {
+        //f(n) = f(n-1)+f(n-2)
+                if(target == 0){
+            return 0;
+        }
+        if(target == 2){
+            return 2;
+        }
+        if(target == 1){
+            return 1;
+        }
+        return RectCover(target -1)+RectCover(target  -2);
+
+    }
+
+    /**
+     * 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+     * @param base
+     * @param exponent
+     * @return
+     */
+    public static double Power(double base, int exponent) {
+        if(exponent == 0){
+            return 1;
+        }
+        double rsl = base;
+//        if(exponent >0){
+//            for(int i = 1;i<exponent;i++){
+//                rsl = rsl*base;
+//            }
+//        } else{
+//                for(int i = exponent+1;i<0;i++){
+//                    System.out.println(rsl+":"+base);
+//                    rsl = rsl*base;
+//
+//                }
+//            }
+        for(int i = 1;i<Math.abs(exponent);i++){
+                rsl = rsl*base;
+        }
+            return exponent >0?rsl:1/rsl;
+    }
+
+    public static double PowerII(double base, int exponent) {
+        if(exponent<0){
+            ;
+        }
+        if(exponent == 1){
+            return base;
+        }
+        if(exponent == 0){
+            return 1;
+        }
+        return exponent>0?base*PowerII(base,Math.abs(exponent)-1):1/(base*PowerII(base,Math.abs(exponent)-1));
+    }
+
+    /**
+     * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，所有的偶数位于数组的后半部分，
+     * 并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+     * @param array
+     */
+    public static void reOrderArray(int [] array) {
+        int[] rsl = new int[array.length];
+        int oddnum = 0;
+        int evennum = array.length-1;
+        int i = 0,j = array.length - 1;
+        while(i<=j){
+            if(array[oddnum]%2 == 1){
+                rsl[i++] = array[oddnum];
+                oddnum++;
+            }else{
+                oddnum++;
+            }
+            if(array[evennum]%2 == 0){
+                rsl[j--] = array[evennum];
+                evennum--;
+            }else{
+                evennum--;
+            }
+
+        }
+        for(int s:rsl){
+            System.out.println(s);
+        }
+    }
+    public static void reOrderArrayII(int [] array) {
+        int i = 0;
+        int firstEvenNum  = -1;
+        int temp;
+        int temp2;
+        int lastOdd;
+        while(i<array.length){
+            if(array[i]%2 == 1){
+
+                if(firstEvenNum != -1){
+                    lastOdd = array[i];
+                    temp = array[firstEvenNum];
+                    for(int n = firstEvenNum;n<i;n++){
+                        temp2 = array[n+1];
+                        array[n+1] = temp;
+                        temp = temp2;
+                    }
+
+                    array[firstEvenNum] = lastOdd;
+                    firstEvenNum++;
+                }
+            }else{
+                if(firstEvenNum == -1){
+                    firstEvenNum = i;
+                }
+            }
+            i++;
+        }
     }
     public static void main(String[] args) {
         //5,[[1,2,8,9],[2,4,9,12],[4,7,10,13],[6,8,11,15]]
@@ -106,8 +318,11 @@ public class Arithmetic1_10 {
 //        for(Integer s:arrayList){
 //            System.out.println(s);
 //        }
-        int[] arr = {6,6,6,6};
-        System.out.println(minNumberInRotateArray(arr));
+//        int[] arr = {6,6,6,6};
+////        System.out.println(minNumberInRotateArray(arr));
+        //System.out.println(RectCover(4));
+        //System.out.println(PowerII(2,1));
+        reOrderArrayII(new int[]{1,2,3,4,5,6,7});
     }
 
 }
