89adae22da33ac3b5c2ba7789a5209d589f5b347
huangyujie <190158792@qq.com>
2019-03-05 13:53:41 +0800

    sortAndData

--- src/main/java/com/hyj/arithmetic/sort/HighLevelSorting.java
+++ src/main/java/com/hyj/arithmetic/sort/HighLevelSorting.java
@@ -49,8 +49,85 @@ public class HighLevelSorting {
         System.out.println("最终排序："+Arrays.toString(array));
     }
 
+
+    public static void QuickSort(int[] array,int left,int right){
+        if (left >=right){
+            return;
+        }
+        int point = partitionIt(array,left,right);
+        QuickSort(array,left,point-1);
+        QuickSort(array,point+1,right);
+    }
+
+    /**
+     * 某一次排序
+     * @param array
+     * @param left
+     * @param right
+     */
+    public static int partitionIt(int[] array,int left,int right){
+        medianOf3(array,left,right);
+        int temp = left;
+        left = left+1;
+        while(left <right){
+            if(array[left]>array[temp] && array[right]<array[temp]){
+                swap(array,left,right);
+            }else{
+                if(array[left]<array[temp]){
+                    left++;
+                }
+                if(array[right]>array[temp]){
+                    right--;
+                }
+            }
+
+        }
+        if(array[left] >array[temp]){
+            swap(array,left-1,temp);
+            return left-1;
+        }else{
+            swap(array,left,temp);
+            return left;
+        }
+    }
+
+    /**
+     * 数组交换位置
+     * @param array
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] array,int i,int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+    //取数组下标第一个数、中间的数、最后一个数的中间值
+    public static int medianOf3(int[] array,int left,int right){
+        int center = (right - left)/2+1;
+        //保证array[left] < array[right]
+        if(array[left] > array[right]){
+            swap(array,left,right);
+        }
+        //保证array[right]最大
+        if(array[center]>array[right]){
+            swap(array,center,right);
+        }
+        if(array[center]>array[left]){
+            swap(array,center,left);
+        }
+        return array[left];
+    }
     public static void main(String[] args) {
-        int[] array = {4,2,8,9,5,7,6,1,3,10};
-        shellKnuthSort(array);
+        int[] array = {9,9,8,7,6,5,4,3,2,1};
+        //shellKnuthSort(array);
+        QuickSort(array,0,array.length-1);
+        //partitionIt(array,0,array.length-1);
+        System.out.println(Arrays.toString(array));
+
+        while (true){
+
+        }
     }
 }
